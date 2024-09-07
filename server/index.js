9cbe4a156039f3454b126ea3d06257f86c087bd1
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-07 18:23:17 +0530

    Project setup complete

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+
+mongoose.connect(MONGO_URI).then(()=>{
+    console.log('MongoDB Connected');
+}).catch(error=>{console.log(error);
+})
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'PUT' ],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Cache-Control',
+        'Expires',
+        'Pragma'
+    ],
+    credentials: true
+}))
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, ()=>{
+    console.log('Server is now running');
+})
