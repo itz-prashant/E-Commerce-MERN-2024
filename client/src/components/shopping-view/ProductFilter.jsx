8102e529702905452dc6c3aa3422145cd7cd8996
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-14 13:37:34 +0530

    Filter functionaliy done

--- client/src/components/shopping-view/ProductFilter.jsx
+++ client/src/components/shopping-view/ProductFilter.jsx
@@ -4,7 +4,7 @@ import { Label } from '../ui/label'
 import { Checkbox } from '../ui/checkbox'
 import { Separator } from '../ui/separator'
 
-const ProductFilter = () => {
+const ProductFilter = ({filter, handlefilter}) => {
   return (
     <div className='bg-background rounded-lg shadow-sm'>
         <div className="p-4 border-b">
@@ -18,7 +18,10 @@ const ProductFilter = () => {
                         <div className="grid gap-2 mt-2">
                             {
                                 filterOption[keyItem].map(option => <Label className='flex items-center gap-2 font-medium'>
-                                    <Checkbox />
+                                    <Checkbox checked={
+                                        filter && Object.keys(filter).length > 0 &&
+                                        filter[keyItem] && filter[keyItem].indexOf(option.id) > -1
+                                    } onCheckedChange={()=> handlefilter(keyItem, option.id)}/>
                                     {option.label}
                                 </Label>)
                             }
--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -5,13 +5,47 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenu
 import { sortOption } from '@/config'
 import { fertchAllFilterProduct } from '@/store/shop/product-slice'
 import { ArrowUpDown } from 'lucide-react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { json } from 'react-router-dom'
 
 const Listing = () => {
 
   const dispatch = useDispatch()
   const {productList} = useSelector(state => state.shopProduct)
+  const [filter, setFilter] = useState({})
+  const [sort, setSort] = useState(null)
+
+  function handleSort(value){
+    console.log(value);
+    setSort(value)
+  }
+
+  function handleFilter(getSectionId, getCurrentOption){
+
+    let copyFilter = {...filter};
+    const indexOfCurrentSection = Object.keys(copyFilter).indexOf(getSectionId)
+    if(indexOfCurrentSection === -1){
+      copyFilter ={
+        ...copyFilter,
+        [getSectionId]: [getCurrentOption]
+      }
+    }else{
+      const indexOfCurrentOption = copyFilter[getSectionId].indexOf(getCurrentOption)
+
+      if(indexOfCurrentOption === -1) copyFilter[getSectionId].push(getCurrentOption) 
+        else{
+          copyFilter[getSectionId].splice(indexOfCurrentOption, 1)
+      }
+    }
+    setFilter(copyFilter)
+    sessionStorage.setItem('filters', JSON.stringify(copyFilter))
+  }
+  
+  useEffect(()=>{
+    setSort("price-lowtohigh")
+    setFilter(JSON.parse(sessionStorage.getItem('filters')) || {})
+  })
 
   useEffect(()=>{
     dispatch(fertchAllFilterProduct())
@@ -19,12 +53,12 @@ const Listing = () => {
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-[270px_1fr] gap-5 p-4 md:p-6'>
-      <ProductFilter />
+      <ProductFilter filter={filter} handlefilter={handleFilter} />
       <div className='bg-background w-full rounded-lg shadow-sm'>
         <div className="p-4 border-b flex items-center justify-between">
           <h2 className='text-lg font-extrabold'>All Products</h2>
           <div className="flex items-center gap-3">
-            <span className="text-muted-foreground">{productList.lengts}</span>
+            <span className="text-muted-foreground">{productList.length} Products</span>
             <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline" size="sm" className="flex items-center gap-1">
@@ -33,9 +67,9 @@ const Listing = () => {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-[200px]">
-                <DropdownMenuRadioGroup>
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
                   {
-                    sortOption.map(sortItem => <DropdownMenuRadioItem key={sortItem.id}>
+                    sortOption.map(sortItem => <DropdownMenuRadioItem value={sortItem.id} key={sortItem.id}>
                       {sortItem.label}
                     </DropdownMenuRadioItem>)
                   }
