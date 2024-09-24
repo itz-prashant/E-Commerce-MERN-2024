045ff9d184bc06739a9b1fbf09e9e27ac34cc4ab
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-24 12:11:20 +0530

    Style payment success page

--- client/src/pages/shopping-view/PaymentSuccess.jsx
+++ client/src/pages/shopping-view/PaymentSuccess.jsx
@@ -1,8 +1,19 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const PaymentSuccess = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>PaymentSuccess</div>
+    <Card className="p-10">
+      <CardHeader className='p-0'>
+        <CardTitle className="text-4xl">Payment Success Full</CardTitle>
+      </CardHeader>
+      <Button className='mt-5' onClick={()=> navigate('/shop/account')}>View Orders</Button>
+    </Card>
   )
 }
 
